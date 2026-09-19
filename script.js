bc5f1ca84d83e0fcbda185640920a17a3e0878a8
cap1007rs-prog <cap1007rs@gmail.com>
2026-09-19 12:07:28 +0530

    Add sidebar navigation functions

---
+++ script.js
@@ -0,0 +1,8 @@
+/* JavaScript Code */
+function openNav() {
+  document.querySelector(".sidebar").style.width = "150px";
+}
+
+function closeNav() {
+  document.querySelector(.sidebar").style.width = "0";
+}
