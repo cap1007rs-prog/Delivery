b27ad3b11a1f5ce19b58585cf6aa04be61c901d7
cap1007rs-prog <cap1007rs@gmail.com>
2026-09-19 12:17:44 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-/* JavaScript Code */
-function openNav() {
-  document.querySelector(".sidebar").style.width = "150px";
-}
-
-function closeNav() {
-  document.querySelector(.sidebar").style.width = "0";
-}
